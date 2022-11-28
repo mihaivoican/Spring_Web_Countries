1a722cc20d03ea8f1421738864285a848c7107b6
Mihai Voican <mihai.voican@rarom.ro>
2022-11-28 16:31:34 +0200

    initial commit spring web

---
+++ src/test/java/com/fastrackit/countriesapplication/CountriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastrackit.countriesapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
